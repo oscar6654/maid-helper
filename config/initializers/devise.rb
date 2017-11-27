8f83315359136ee571b415d7fc2855d22694cda7
OSCAR <ochng6654@gmail.com>
2017-11-27 17:18:04 +0800

    added callback

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -251,7 +251,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, ENV['APP_ID'], ENV['APP_SECRET'], callback_url: "http://localhost:5000/users/auth/facebook/callback"
+  config.omniauth :facebook, ENV['APP_ID'], ENV['APP_SECRET'], callback_url: "https://#{ENV['FACEBOOK_APP_HOST']}/u/auth/facebook/callback"
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
   # ENV['COMPANY_NAME']
   # ==> Warden configuration
